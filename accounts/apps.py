a36e2a68de7d1b7dd81ee8dda98462764453bfb9
by-vshrd <egormipmip@gmail.com>
2026-06-13 11:58:15 +0700

    Добавлены миграции с полем protocol

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.db import connection
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # Исправляем отсутствующее поле protocol в уже существующей таблице
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("ALTER TABLE accounts_profile ADD COLUMN IF NOT EXISTS protocol varchar(10) DEFAULT 'hysteria'")
+        except Exception:
+            pass
